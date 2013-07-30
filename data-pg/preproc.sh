72c753e49a2f6b03c8699c428ba7fb45926c6739
Alex Clemmer <clemmer.alexander@gmail.com>
2013-07-30 18:12:18 -0400

    Cause `preproc.sh` to rename all the log files before preproc'ing

--- data-pg/preproc.sh
+++ data-pg/preproc.sh
@@ -3,4 +3,9 @@
 # specified by Philip Guo, and puts all that in one file. One line per
 # query string.
 
+for file in *.log*
+do
+	mv "$file" "${file/.log/}.log"
+done
+
 cat *.log | grep -e "web_exec_py[23]\(-1\)\?.py" | grep -oP "user_script=.*? " > all.txt
