c75e75eb2932e91a013425c658ad64e8802239e3
Alex Clemmer <clemmer.alexander@gmail.com>
2013-07-30 15:50:57 -0400

    Comment shell script, add shebang

--- data-pg/preproc.sh
+++ data-pg/preproc.sh
@@ -1,1 +1,6 @@
+#!/bin/bash
+# Cats logs together, strips everything not matching the querystring format
+# specified by Philip Guo, and puts all that in one file. One line per
+# query string.
+
 cat *.log | grep -e "web_exec_py[23]\(-1\)\?.py" | grep -oP "user_script=.*? " > all.txt
